d4893fb2d8e255dcd95600253b8ddf5719ae8d0f
polinatrump <polinatrump@mail.ru>
2023-02-11 18:57:35 +0100

    code fixed

--- src/repository/book_repository.py
+++ src/repository/book_repository.py
@@ -25,7 +25,6 @@ class BookRepository(SqlAlchemyRepository):
             return self.parse(model)
         return model
 
-
 if __name__ == '__main__':
     session = get_session()
     BookRepository(session).create(
